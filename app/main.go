2c2a89f47043afd947e227859a645b1aa1d6e489
MinRayLin <minray.lin@cloudnifier.com>
2026-03-15 20:17:23 +0800

    windows案enter是\r\n，要用strings.TrimSpace才可以把123顯示出來

--- app/main.go
+++ app/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
 func main() {
-	fmt.Print("$ ")
+	comment, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
+	userinput := strings.TrimSpace(comment)
+	fmt.Print(userinput + ": command not found")
 }
