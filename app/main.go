b3dc90c05694e895e30f9487a811dc896ba24ca6
MinRayLin <minray.lin@cloudnifier.com>
2026-03-15 20:29:47 +0800

    implement exit to break the loop

--- app/main.go
+++ app/main.go
@@ -18,7 +18,12 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
+
 		userinput := strings.TrimSpace(comment)
+		if userinput == "exit" {
+			break
+		}
+
 		fmt.Println(userinput + ": command not found")
 	}
 }
